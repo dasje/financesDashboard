b3ac0485c5fa029197288fdd4e8e5189d948fbea
Ben <kwalijkje@live.com>
2024-04-13 09:02:02 +0200

    INit commit spring initialiser

---
+++ fd-backend-java/src/main/java/com/bensweeney/financedashboard/financedashboard/FinancedashboardApplication.java
@@ -0,0 +1,13 @@
+package com.bensweeney.financedashboard.financedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancedashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancedashboardApplication.class, args);
+	}
+
+}
