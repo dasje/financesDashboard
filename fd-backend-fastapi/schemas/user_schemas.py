571ff8ab47a208568c786caa2d181f98a98693e5
Ben <kwalijkje@live.com>
2024-04-16 17:06:58 +0200

    User schema with email and password validation added

---
+++ fd-backend-fastapi/schemas/user_schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, ValidationError
+from pydantic.functional_validators import AfterValidator
+import re
+from typing_extensions import Annotated
+
+def validate_email(value):
+    pattern = r'^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$'
+    assert re.match(pattern, value), "Invalid email format."
+    return value
+
+def validate_password(value):
+    min_max_length = r'^[\s\S]{8,32}$'
+    upper = r'[A-Z]'
+    lower = r'[a-z]'
+    number = r'[0-9]'
+    special = r'[ !"#$%&\'()*+,\-./:;<=>?@[\\\]^_`{|}~]'
+
+    assert re.search(min_max_length, value), "Password is the wrong length."
+    assert re.search(upper, value), "Password does not contain upper case character(s)." 
+    assert re.search(lower, value), "Password does not contain lower case character(s)." 
+    assert re.search(number, value), "Password does not contain digit(s)." 
+    assert re.search(special, value), "Password does not contain special character(s)."
+    return value
+
+
+class User(BaseModel):
+    email: Annotated[str, AfterValidator(validate_email)]
+    password: Annotated[str, AfterValidator(validate_password)]
+
+    class Config:
+        orm_mode = True
+    
+
