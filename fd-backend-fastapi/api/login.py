6e13a603ffe159566823ffa8c0a8ba6a30c13559
Ben <kwalijkje@live.com>
2024-04-16 20:24:55 +0200

    Added logic to authenticate user and create access token

--- fd-backend-fastapi/api/login.py
+++ fd-backend-fastapi/api/login.py
@@ -1,19 +1,134 @@
-from schemas.user_schemas import User
-from schemas.response_schemas import BaseResponse, OutgoingMessage
-from models.models import Users
+
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Union
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 from passlib.context import CryptContext
-from datetime import datetime
-from crud.general_crud import add_item, get_item
-from fastapi import HTTPException
+
+from crud.general_crud import (
+    add_item,
+    get_item
+)
+from schemas.user_schemas import (
+    User,
+    Token,
+    TokenData
+)
+from schemas.response_schemas import (
+    BaseResponse,
+    OutgoingMessage
+)
+from models.models import Users
+
+from config import (
+    SECRET_KEY,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ALGORITHM,
+)
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Use CryptContext to verify entered passport against stored hash.
+
+    Args:
+        plain_password (str):
+        hashed_password (str):
+
+    Returns:
+        bool:
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
+    """
+    Use CryptContext to generate a hash for the entered password.
+
+    Args:
+        password (str)
+
+    Returns:
+        str
+    """
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
+    """
+    Generate an access token with expiry time.
+
+    Args:
+        data (dict)
+        expires_delta (Union[timedelta, None], optional). Defaults to None.
+
+    Returns:
+        str
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(email: str, password: str) -> Users:
+    """
+    Confirm user registered in db.
+    Veryify entered password is correct.
+
+    Args:
+        email (str)
+        password (str)
+
+    Returns:
+        Users
+    """
+    user = get_item(Users, [Users.email == email])
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> dict:
+    """
+    Args:
+        token (Annotated[str, Depends)
+
+    Raises:
+        credentials_exception
+
+    Returns:
+        dict
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_email: str = payload.get("sub")
+        if user_email is None:
+            raise credentials_exception
+        token_data = TokenData(user_email=user_email)
+    except JWTError:
+        raise credentials_exception
+    user = get_item(Users, [Users.email == token_data.user_email])
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 def sign_up_user(user: User) -> BaseResponse:
@@ -41,10 +156,26 @@ def sign_up_user(user: User) -> BaseResponse:
     return HTTPException(500, detail=OutgoingMessage.user_not_added.value)
 
     
-def login_user(user: User) -> BaseResponse:
-    if user.email:
-        r = BaseResponse(message="Email correct.")
-        return r
-    else:
-        r = BaseResponse(message="No valid email.")
-        return r
+def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> BaseResponse:
+    """
+    Args:
+        form_data (Annotated[OAuth2PasswordRequestForm, Depends)
+
+    Raises:
+        HTTPException
+
+    Returns:
+        BaseResponse
+    """
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
