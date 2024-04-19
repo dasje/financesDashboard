2916f39f45e9d797390ca489f9bc6cc6362781b5
Ben <kwalijkje@live.com>
2024-04-19 12:46:54 +0200

    Login component added

--- fd-frontend-react/src/components/login.tsx
+++ fd-frontend-react/src/components/login.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import type { FormProps } from "antd";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Alert, Button, Checkbox, Form, Input } from "antd";
 import { Card, Space } from "antd";
-// import endpoint from "../endpoints.config";
+import { ValidateStatus } from "antd/es/form/FormItem";
+import { handleLogin } from "../api/auth";
+import { useNavigate } from "react-router-dom";
 
 type FieldType = {
   username?: string;
   password?: string;
   remember?: string;
 };
+
+const emailRegex: RegExp = /^[\w\-.]+@([\w-]+\.)+[\w-]{2,}$/;
+const minMaxLength: RegExp = /^[\s\S]{8,32}$/;
+const upper: RegExp = /[A-Z]/;
+const lower: RegExp = /[a-z]/;
+const number: RegExp = /[0-9]/;
+const special: RegExp = /[ !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
 
 const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
   console.log("Success:", values);
@@ -18,13 +27,77 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
 
-function Login() {
-  //   var signup_link: string = endpoint.signUpUrl;
+const Login = () => {
+  const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
+  const [emailEnterStatus, setEmailEnterStatus] = useState<ValidateStatus>("");
+  const [passwordEnterStatus, setPasswordEnterStatus] =
+    useState<ValidateStatus>("");
+  const [showErrorMessage, setShowErrorMessage] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const emailValidation = (email: string) => {
+    return emailRegex.test(email);
+  };
+  const passwordValidation = (password: string) => {
+    if (
+      minMaxLength.test(password) &&
+      upper.test(password) &&
+      lower.test(password) &&
+      number.test(password) &&
+      special.test(password)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUserEmail(e.target.value);
+    if (!emailValidation(e.target.value)) {
+      setEmailEnterStatus("warning");
+    } else {
+      setEmailEnterStatus("success");
+    }
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUserPassword(e.target.value);
+    if (!passwordValidation(e.target.value)) {
+      setPasswordEnterStatus("warning");
+    } else {
+      setPasswordEnterStatus("success");
+    }
+  };
+
+  const handleSubmitButton = async () => {
+    if (emailValidation(userEmail) && passwordValidation(userPassword)) {
+      console.log("Everything checks out");
+      const res = await handleLogin({
+        email: userEmail,
+        password: userPassword,
+      });
+      switch (res) {
+        case "success":
+          navigate("/no-data");
+          break;
+        case "Incorrect email or password.":
+          setErrorMessage(res);
+          setShowErrorMessage(true);
+          break;
+      }
+    } else {
+      console.log("Cannot proceed");
+    }
+  };
   return (
     <Space direction="vertical" size={16}>
       <Card
         title="Login"
-        extra={<a href={"www.google.com"}>Sign Up</a>}
+        extra={<a href={"/signup"}>Sign Up</a>}
         style={{ width: 300 }}
       >
         <Form
@@ -38,19 +111,29 @@ function Login() {
           autoComplete="off"
         >
           <Form.Item<FieldType>
-            label="Username"
+            label="Email"
             name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+            validateStatus={emailEnterStatus}
+            hasFeedback
+            rules={[{ required: true, message: "Please input your email!" }]}
           >
-            <Input />
+            <Input
+              onChange={(e) => handleNameChange(e)}
+              id={emailEnterStatus}
+            />
           </Form.Item>
 
           <Form.Item<FieldType>
             label="Password"
             name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            validateStatus={passwordEnterStatus}
+            hasFeedback
+            rules={[{ required: true, message: "Please enter a password!" }]}
           >
-            <Input.Password />
+            <Input.Password
+              onChange={(e) => handlePasswordChange(e)}
+              id={passwordEnterStatus}
+            />
           </Form.Item>
 
           <Form.Item<FieldType>
@@ -61,8 +144,18 @@ function Login() {
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
+          {showErrorMessage && (
+            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+              <Alert showIcon={true} message={errorMessage} type="error" />
+            </Form.Item>
+          )}
+
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={handleSubmitButton}
+            >
               Submit
             </Button>
           </Form.Item>
@@ -70,6 +163,6 @@ function Login() {
       </Card>
     </Space>
   );
-}
+};
 
 export default Login;
