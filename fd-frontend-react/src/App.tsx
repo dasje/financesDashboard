f7787c614688af84b24513abddb4592673415681
Ben <kwalijkje@live.com>
2024-04-19 12:47:07 +0200

    Router for login added to App.tsx

--- fd-frontend-react/src/App.tsx
+++ fd-frontend-react/src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/login";
-// import * as dotenv from "dotenv";
+import SignUp from "./components/signup";
 
-// dotenv.config();
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import NoDataPage from "./components/noData";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Login}></Route>
+          <Route path="/login" Component={Login}></Route>
+          <Route path="/signup" Component={SignUp}></Route>
+          <Route path="/no-data" Component={NoDataPage}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
